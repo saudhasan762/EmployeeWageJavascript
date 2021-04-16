917193f697f1fdac02e7cd7c50cd625b8720e1ce
saudhasan762 <azanali152@gmail.com>
2021-04-16 14:55:10 +0530

    Added: calculating daily employee wage for part time

--- employee_Wage.js
+++ employee_Wage.js
@@ -11,16 +11,26 @@ const checkAttendence = () => {
 }
 
 const calcWage = () => {
-    const IS_ABSENT = 0
-    const wage_per_hour = 20;
-    let empHr = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if (empCheck == IS_ABSENT){
-        empHr = 8;
-    }else{
-        empHr = 0;
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOUR = 4;
+    const FULL_TIME_HOUR = 8;
+    const WAGE_PER_HOUR = 20;
+
+    let empHR = 0;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHR = PART_TIME_HOUR;
+            break;
+        case IS_FULL_TIME:
+            empHR = FULL_TIME_HOUR;
+            break;
+        default:
+            empHR = 0;
     }
-    let empWage = empHr * wage_per_hour;
-    console.log("Employee Wage: ", empWage);
+
+    let empWage = empHR * WAGE_PER_HOUR;
+    console.log("Employee Wage: "+ empWage);
 }
 calcWage();
