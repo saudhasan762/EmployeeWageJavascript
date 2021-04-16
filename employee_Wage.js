3695f8921eab183505e38056c7197f02f5b0591f
saudhasan762 <azanali152@gmail.com>
2021-04-16 23:41:41 +0530

    Added: Storing the daily wage along with the day

--- employee_Wage.js
+++ employee_Wage.js
@@ -1,5 +1,13 @@
 console.log("Welcome to Employee Wage Computation program");
 
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_DAYS = 20;
+const MAX_WORKING_HOUR = 100;
+
 const checkAttendence = () => {
     const IS_ABSENT = 0;
     let empCheck = Math.floor(Math.random() * 10) % 2;
@@ -11,12 +19,6 @@ const checkAttendence = () => {
 }
 
 const calcWage = () => {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOUR = 4;
-    const FULL_TIME_HOUR = 8;
-    const WAGE_PER_HOUR = 20;
-
     let empHR = 0;
     let empCheck = Math.floor(Math.random()*10)%3;
     switch(empCheck){
@@ -35,13 +37,6 @@ const calcWage = () => {
 }
 
 const calWageForMonth = () => {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
-    const NUM_OF_WORKING_DAYS = 20;
-
     function getWorkingHours(empCheck){
         switch (empCheck){
             case IS_PART_TIME:
@@ -64,14 +59,6 @@ const calWageForMonth = () => {
 } 
 
 const calcWageForConditon = () => {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
-    const NUM_OF_WORKING_DAYS = 20;
-    const MAX_HRS_IN_MONTH = 100;
-
     function getWorkingHours(empCheck){
         switch (empCheck){
             case IS_PART_TIME:
@@ -92,46 +79,47 @@ const calcWageForConditon = () => {
     }
 
     let empWage = totalEmpHrs * WAGE_PER_HOUR;
-    console.log("Total Days: "+ totalWorkingDays +", Total Employee Hours: "+ totalEmpHrs +", Total Employee Wage : " + empWage);
-}
+    console.log("Total Working Days: "+ totalWorkingDays +", Total Employee Hours: "+ totalEmpHrs +", Total Employee Wage : " + empWage);
+}     
 
-const calcWageUC8 = () => {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
-    const NUM_OF_WORKING_DAYS = 20;
-    const MAX_HRS_IN_MONTH = 160;
-    let empDailyWageArr = new Array();
-    let empHrs;
+const newEmpWage = () => {
     function getWorkingHours(empCheck){
-        switch (empCheck){
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
+        switch(empCheck){
+        case IS_PART_TIME:
+                return PART_TIME_HOUR;
             case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
+            return FULL_TIME_HOUR;
             default:
                 return 0;
         }
     }
 
-    function calculatingDailyWage(){
+    function calcDailyWage(empHrs) {
         return empHrs * WAGE_PER_HOUR;
     }
-    let totalEmpHrs = 0;
+    let totalEmpHr = 0;
     let totalWorkingDays = 0;
-    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    let empDailyHrsAndWageArr = new Array();
+
+    while(totalEmpHr <= MAX_WORKING_HOUR && totalWorkingDays < NUM_OF_DAYS){
         totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
-        empHrs = getWorkingHours(empCheck);
-        totalEmpHrs += empHrs;
-        empDailyWageArr.push(calculatingDailyWage(empHrs));
+        let empHrs = getWorkingHours(empCheck); 
+        totalEmpHr +=empHrs;
+        empDailyHrsAndWageArr.push(
+            {
+                dayNum:totalWorkingDays,
+                dailyHours:empHrs,
+                dailyWage: calcDailyWage(empHrs),
+                toString() {
+                    return '\nDay' +this.dayNum + ' => Working Hours : ' + this.dailyHours +
+                            ' And Wage Earned : ' + this.dailyWage
+                },
+            }
+        );
     }
-
-    let empWage = calculatingDailyWage(totalEmpHrs);
-    console.log("Total Days: "+ totalWorkingDays +", Total Employee Hours: "+ totalEmpHrs +", Total Employee Wage : " + empWage);
-    console.log("Array of daily Wage: ", empDailyWageArr);
+    let empWage = calcDailyWage(totalEmpHr);
+    console.log("Showing Daily Hours Worked And Wage Earned: "+empDailyHrsAndWageArr);
+    console.log("Total Working Days: "+ totalWorkingDays +", Total Employee Hours: "+ totalEmpHr +", Total Employee Wage : " + empWage);
 }
-
-calcWageUC8();
+newEmpWage();
